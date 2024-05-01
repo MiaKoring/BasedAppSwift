e90a33c150c4ee58cd06cb267e4d7d6e56b02a76
Mia <koring.mia@gmail.com>
2024-05-01 14:42:37 +0200

    save

--- Sources/basedAppSwift/NetworkHandler.swift
+++ Sources/basedAppSwift/NetworkHandler.swift
@@ -1,5 +1,6 @@
 import Alamofire
 import Foundation
+import SwiftUI
 
 public class NetworkHandler {
     private var evaluators: [String: ServerTrustEvaluating]
@@ -16,6 +17,7 @@ public class NetworkHandler {
         print(Bundle.main.af.certificates)
     }
     
+    ///sends a usercreation request
     public func createUser(_ session: Session, loginMethod: LoginMethod, recoveryCodes: [String], completion: @escaping (Result<String, Error>) -> Void) {
         let jsonParameters = ["recoveryCodes": recoveryCodes]
         
@@ -29,6 +31,7 @@ public class NetworkHandler {
         }
     }
     
+    ///tries to register a device
     public func loginWithDevice(_ session: Session, loginMethod: LoginMethod, login: Login, publicKey: String, deviceID: String, completion: @escaping (Result<String, Error>) -> Void){
         switch login{
         case .TOTP(let id, let code):
@@ -45,6 +48,40 @@ public class NetworkHandler {
         case .mail(let mail, let password):
             break
         }
+    }
+    //TODO:
+    public func sendMessage(_ session: Session, token: String, completion: @escaping (Result<String, Error>) -> Void) async throws {
+        session.request("https://\(self.host)/messages/send", method: .post, headers: HTTPHeaders([.authorization(bearerToken: token)])).responseString { response in
+            switch response.result {
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+            
+    }
+    
+    public func downloadAttachment(_ session: Session, token: String, fileUUID: String, progressBinding: @Binding Double?)async {
+        let downloadURL = "https://\(self.host)/files/\(fileUUID)"
+           let destination: DownloadRequest.Destination = { _, _ in
+               let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+               let fileURL = documentsURL.appendingPathComponent("\(fileUUID).txt")
+               return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+           }
+
+           do {
+               let response = try await AF.download(downloadURL, to: destination)
+                   .downloadProgress { progress in
+                       progressBinding.wrappedValue = progress.fractionCompleted
+                       print("Download Progress: \(progress.fractionCompleted)")
+                   }
+                   .response(){response in
+                       debugPrint(response)
+                   }
+           } catch {
+               print("Error downloading file: \(error)")
+           }
     }
     
 }
